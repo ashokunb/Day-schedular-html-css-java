ad5feb95e7606de54e3ca841e16b791963b729ec
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-04-02 21:35:50 -0500

    created logic for local storage

--- Assets/script.js
+++ Assets/script.js
@@ -3,14 +3,26 @@ $('#todaysDate').text(moment().format('MMM Do YY, h:mm:ss a'))
 $('#todaysday').text(moment().format('dddd'))
 
 
-var inputTextarea1 = $('#text1')
-console.log(inputTextarea1);
+function makeButtons() {
+    for(i=0; i<9; i++){
+        var currentInput = $('text' +i);
+        $('#button' + [i]).click(function(event){
+            event.preventDefault()
+            localStorage.setItem('content' + i ,currentInput.val());
+            console.log(currentInput);
+        });
+    };
 
-$('button').click(function(event){
-    event.preventDefault()
-    localStorage.setItem('content1',inputTextarea1.val());
-    inputTextarea1.text(inputTextarea1.val());
-})
+};
+
+$('text1').val(localStorage.getItem('content1'));
+$('text1').val(localStorage.getItem('content2'));
+$('text1').val(localStorage.getItem('content3'));
+$('text1').val(localStorage.getItem('content4'));
+$('text1').val(localStorage.getItem('content5'));
+$('text1').val(localStorage.getItem('content6'));
+$('text1').val(localStorage.getItem('content7'));
+$('text1').val(localStorage.getItem('content8'));
 
 
 
