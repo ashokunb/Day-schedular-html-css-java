5c692323cd90f9d9f631cf972bd635aa9609bba7
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-04-02 20:45:04 -0500

    changed the id's for the text area

--- Assets/script.js
+++ Assets/script.js
@@ -3,12 +3,13 @@ $('#todaysDate').text(moment().format('MMM Do YY, h:mm:ss a'))
 $('#todaysday').text(moment().format('dddd'))
 
 
-var inputTextarea1 = $('#text')
+var inputTextarea1 = $('#text1')
 console.log(inputTextarea1);
 
 $('button').click(function(event){
     event.preventDefault()
-    localStorage.setItem('')
+    localStorage.setItem('content1',inputTextarea1.val());
+    inputTextarea1.text(inputTextarea1.val());
 })
 
 
