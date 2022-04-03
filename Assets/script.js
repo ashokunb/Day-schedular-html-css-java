e0dbefecf11e50bd507f6800b06b7adb444a8183
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-04-02 20:34:50 -0500

    creating local storage

--- Assets/script.js
+++ Assets/script.js
@@ -3,6 +3,14 @@ $('#todaysDate').text(moment().format('MMM Do YY, h:mm:ss a'))
 $('#todaysday').text(moment().format('dddd'))
 
 
+var inputTextarea1 = $('#text')
+console.log(inputTextarea1);
+
+$('button').click(function(event){
+    event.preventDefault()
+    localStorage.setItem('')
+})
+
 
 
 
